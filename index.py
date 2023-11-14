30d6d424075eafe728a07971f6fffbb793c8cf4e
zhu-wenxiang <zwx199288@163.com>
2023-11-14 17:27:48 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,10 +10,23 @@ def call_service():
 
 @route('/')
 def index():
+	#Start tracemalloc
+	tracemalloc.start()
+	
     """Home page"""
     title = "Image Processor App"
     call_service()
-    return template('index.tpl',data="Request completed!", title=title)
+	
+	snapshot = tracemalloc.take_snapshot() # Taking snapshot
+    top_stats = snapshot.statistics('lineno') # Getting statistics
+
+ 	# ... Printing to console ...
+    print("[ Top 10 ]") 
+    for stat in top_stats[:10]:
+        print(stat)
+    # ... Printing to console ...
+	
+		return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
 	run(host='0.0.0.0', port=8000, debug=False, reloader=True)
